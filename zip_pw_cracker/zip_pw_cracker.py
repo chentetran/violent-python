1972e463a4f30d0c52ae14f8a7ecf0ca3718c320
Vincent Tran <vincenttran23@gmail.com>
2016-08-03 02:39:23 -0400

    adding theading

--- zip_pw_cracker/zip_pw_cracker.py
+++ zip_pw_cracker/zip_pw_cracker.py
@@ -1,13 +1,18 @@
 import zipfile
+from threading import Thread
 
-zFile = zipfile.ZipFile('evil.zip')
-pwFile = open('dictionary.txt')
-
-for line in pwFile.readlines():
-	pw = line.strip('\n')
+def extractFile(zFile, pw):
 	try:
 		zFile.extractall(pwd=pw)
 		print '[+] Password is ' + pw
 		exit(0)
 	except Exception, e:
-		pass
+			pass
+			
+zFile = zipfile.ZipFile('evil.zip')
+pwFile = open('dictionary.txt')
+
+for line in pwFile.readlines():
+	pw = line.strip('\n')
+	t = Thread(target=extractFile, args=(zFile, pw))
+	t.start()
