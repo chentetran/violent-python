e0a7132128fd23d12b82d02d9899a2ff51a836b5
Vincent Tran <vincenttran23@gmail.com>
2016-08-03 02:47:40 -0400

    user can now specify zip file and dictionary to use

--- zip_pw_cracker/zip_pw_cracker.py
+++ zip_pw_cracker/zip_pw_cracker.py
@@ -1,4 +1,5 @@
 import zipfile
+import optparse
 from threading import Thread
 
 def extractFile(zFile, pw):
@@ -7,12 +8,24 @@ def extractFile(zFile, pw):
 		print '[+] Password is ' + pw
 		exit(0)
 	except Exception, e:
-			pass
-			
-zFile = zipfile.ZipFile('evil.zip')
-pwFile = open('dictionary.txt')
+		pass
 
-for line in pwFile.readlines():
-	pw = line.strip('\n')
-	t = Thread(target=extractFile, args=(zFile, pw))
-	t.start()
+parser = optparse.OptionParser('usage%prog -f <zipfile> -d <dictionary>')
+parser.add_option('-f', dest='zname', type='string', help='specify zip file')
+parser.add_option('-d', dest='dname', type='string', help='specify dictionary file')
+(options, args) = parser.parse_args()
+
+if (options.zname == None) | (options.dname == None):
+	print parser.usage
+	exit(0)
+else:
+	zname = options.zname
+	dname = options.dname
+
+	zFile = zipfile.ZipFile(zname)
+	pwFile = open(dname)
+
+	for line in pwFile.readlines():
+		pw = line.strip('\n')
+		t = Thread(target=extractFile, args=(zFile, pw))
+		t.start()
